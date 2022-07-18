719725a29b71490021ddf91e353b6a7027051f9f
Perennial2002 <pendragon05082002@gmail.com>
2022-07-18 11:39:39 +0500

    updated

--- app/src/main/java/com/example/android_graphql/fragment/SecondFragment.kt
+++ app/src/main/java/com/example/android_graphql/fragment/SecondFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.apollographql.apollo3.exception.ApolloException
 import com.example.android_graphql.*
 import com.example.android_graphql.databinding.FragmentSecondBinding
@@ -18,7 +20,7 @@ class SecondFragment : Fragment(R.layout.fragment_second) {
 
     private lateinit var _binding: FragmentSecondBinding
     private lateinit var userId: String
-    private lateinit var user: UsersListQuery.User
+    private lateinit var user: UserByIdQuery.Users_by_pk
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,12 +38,18 @@ class SecondFragment : Fragment(R.layout.fragment_second) {
     private fun getUserById(userId: String) {
         lifecycleScope.launch {
             val response = try {
-                GraphQL.get().query(UserByIdQuery(userId)).execute()
+                GraphQL.get().query(UserByIdQuery(userId)).execute().data?.users_by_pk
             } catch (e: ApolloException) {
                 Log.d("TAG", "insertUser: ${e.localizedMessage}")
             }
 
             Log.d("TAG", "getUserById: $response")
+            user = response as UserByIdQuery.Users_by_pk
+            _binding.apply {
+                edtName.setText(user.name)
+                edtRocket.setText(user.rocket)
+                edtTwitter.setText(user.twitter)
+            }
         }
     }
 
@@ -58,18 +66,24 @@ class SecondFragment : Fragment(R.layout.fragment_second) {
         _binding.apply {
             if (userId.isNotEmpty()) {
                 btnDelete.visibility = View.VISIBLE
-
-//                edtName.setText(user.name)
-//                edtRocket.setText(user.rocket)
-//                edtTwitter.setText(user.twitter)
+                btnSave.text = "Update"
             }
 
             btnSave.setOnClickListener {
-                insertUser(
-                    edtName.text.toString(),
-                    edtRocket.text.toString(),
-                    edtTwitter.text.toString()
-                )
+                if (userId.isNotEmpty())
+                    updateUser(
+                        userId,
+                        edtName.text.toString(),
+                        edtRocket.text.toString(),
+                        edtTwitter.text.toString()
+                    )
+                else {
+                    insertUser(
+                        edtName.text.toString(),
+                        edtRocket.text.toString(),
+                        edtTwitter.text.toString()
+                    )
+                }
             }
 
             btnDelete.setOnClickListener {
@@ -78,9 +92,11 @@ class SecondFragment : Fragment(R.layout.fragment_second) {
         }
     }
 
+
     private fun insertUser(name: String, rocket: String, twitter: String) {
         lifecycleScope.launch launchWhenResumed@{
             val result = try {
+                back("Saved bro!!")
                 GraphQL.get().mutation(
                     InsertUserMutation(name, rocket, twitter)
                 ).execute()
@@ -92,9 +108,15 @@ class SecondFragment : Fragment(R.layout.fragment_second) {
         }
     }
 
+    private fun back(msg: String) {
+        Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+        findNavController().popBackStack()
+    }
+
     private fun updateUser(id: String, name: String, rocket: String, twitter: String) {
         lifecycleScope.launch launchWhenResumed@{
             val result = try {
+                back("Updated bro!!!")
                 GraphQL.get().mutation(
                     UpdateUserMutation(id, name, rocket, twitter)
                 ).execute()
@@ -109,6 +131,7 @@ class SecondFragment : Fragment(R.layout.fragment_second) {
     private fun deleteUser(id: String) {
         lifecycleScope.launch launchWhenResumed@{
             val result = try {
+                back("Deleted bro!!!")
                 GraphQL.get().mutation(DeleteUserMutation(id)).execute()
             } catch (e: ApolloException) {
                 Log.d("MainActivity", e.toString())
